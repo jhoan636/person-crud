2b1cf6e5ee30517b8316faae639be1f685aa3e83
jhoan636 <jhoan_londono82162@elpoli.edu.co>
2025-09-08 15:30:52 -0500

    Initialize project with Spring Boot configuration and Maven wrapper. Add application properties, dependencies, and initial project structure.

---
+++ src/main/java/com/cohan/personal_crud/PersonalCrudApplication.java
@@ -0,0 +1,13 @@
+package com.cohan.personal_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cohan/personal_crud/PersonalCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohan.personal_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
