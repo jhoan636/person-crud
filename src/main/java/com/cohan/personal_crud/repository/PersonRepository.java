d48d93c5bb6e82d7b878ce4c37a66a8ecc99b140
jhoan636 <jhoan.63@hotmail.com>
2025-09-09 07:39:15 -0500

    Add `PersonRepository` interface for data access

---
+++ src/main/java/com/cohan/personal_crud/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.cohan.personal_crud.repository;
+
+import com.cohan.personal_crud.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
