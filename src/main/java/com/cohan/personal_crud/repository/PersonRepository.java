eb0c0bee5c1608c84e23a565e25751cd27b044a2
jhoan636 <jhoan.63@hotmail.com>
2025-09-09 22:34:27 -0500

    Add method to find persons by first name ignoring case in PersonRepository

--- src/main/java/com/cohan/personal_crud/repository/PersonRepository.java
+++ src/main/java/com/cohan/personal_crud/repository/PersonRepository.java
@@ -4,7 +4,11 @@ import com.cohan.personal_crud.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByFirstNameContainingIgnoreCase(String firstName);
 
 }
